d840933f3308ac9dc6d701e90da19e0e310ecb0a
Erhan ÜRGÜN <erhan.rgn04@gmail.com>
2022-03-28 18:30:54 +0300

    ilk yükleme

---
+++ PaintKutuphanesi/Cizim.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace PaintKutuphanesi
+{
+    public class Cizim
+    {
+    
+    }
+}
---
+++ PaintUygulamasi/frmPaint.Designer.cs
@@ -0,0 +1,298 @@
+﻿
+namespace PaintUygulamasi
+{
+    partial class frmPaint
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPaint));
+            this.gboxCizimPaneli = new System.Windows.Forms.GroupBox();
+            this.pboxCizimAlani = new System.Windows.Forms.PictureBox();
+            this.gboxAracPaneli = new System.Windows.Forms.GroupBox();
+            this.gboxAyarlarPaneli = new System.Windows.Forms.GroupBox();
+            this.pboxKalemRengi = new System.Windows.Forms.PictureBox();
+            this.pboxSekilIci = new System.Windows.Forms.PictureBox();
+            this.chbIciniDoldur = new System.Windows.Forms.CheckBox();
+            this.pboxSekilKenari = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numTxtKalinlik = new System.Windows.Forms.NumericUpDown();
+            this.btnSerbestCizim = new System.Windows.Forms.Button();
+            this.btnDuzCizgi = new System.Windows.Forms.Button();
+            this.btnDaire = new System.Windows.Forms.Button();
+            this.btnUcgen = new System.Windows.Forms.Button();
+            this.btnDortgen = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.gboxCizimPaneli.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxCizimAlani)).BeginInit();
+            this.gboxAracPaneli.SuspendLayout();
+            this.gboxAyarlarPaneli.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxKalemRengi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxSekilIci)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxSekilKenari)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numTxtKalinlik)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gboxCizimPaneli
+            // 
+            this.gboxCizimPaneli.Controls.Add(this.pboxCizimAlani);
+            this.gboxCizimPaneli.Location = new System.Drawing.Point(9, 5);
+            this.gboxCizimPaneli.Name = "gboxCizimPaneli";
+            this.gboxCizimPaneli.Size = new System.Drawing.Size(569, 486);
+            this.gboxCizimPaneli.TabIndex = 0;
+            this.gboxCizimPaneli.TabStop = false;
+            this.gboxCizimPaneli.Text = " Çizim Paneli ";
+            // 
+            // pboxCizimAlani
+            // 
+            this.pboxCizimAlani.BackColor = System.Drawing.Color.White;
+            this.pboxCizimAlani.Location = new System.Drawing.Point(7, 18);
+            this.pboxCizimAlani.Name = "pboxCizimAlani";
+            this.pboxCizimAlani.Size = new System.Drawing.Size(555, 459);
+            this.pboxCizimAlani.TabIndex = 0;
+            this.pboxCizimAlani.TabStop = false;
+            // 
+            // gboxAracPaneli
+            // 
+            this.gboxAracPaneli.Controls.Add(this.gboxAyarlarPaneli);
+            this.gboxAracPaneli.Controls.Add(this.btnSerbestCizim);
+            this.gboxAracPaneli.Controls.Add(this.btnDuzCizgi);
+            this.gboxAracPaneli.Controls.Add(this.btnDaire);
+            this.gboxAracPaneli.Controls.Add(this.btnUcgen);
+            this.gboxAracPaneli.Controls.Add(this.btnDortgen);
+            this.gboxAracPaneli.Location = new System.Drawing.Point(586, 5);
+            this.gboxAracPaneli.Name = "gboxAracPaneli";
+            this.gboxAracPaneli.Size = new System.Drawing.Size(203, 486);
+            this.gboxAracPaneli.TabIndex = 0;
+            this.gboxAracPaneli.TabStop = false;
+            this.gboxAracPaneli.Text = " Araçlar Paneli ";
+            // 
+            // gboxAyarlarPaneli
+            // 
+            this.gboxAyarlarPaneli.Controls.Add(this.pboxKalemRengi);
+            this.gboxAyarlarPaneli.Controls.Add(this.pboxSekilIci);
+            this.gboxAyarlarPaneli.Controls.Add(this.chbIciniDoldur);
+            this.gboxAyarlarPaneli.Controls.Add(this.pboxSekilKenari);
+            this.gboxAyarlarPaneli.Controls.Add(this.label3);
+            this.gboxAyarlarPaneli.Controls.Add(this.label2);
+            this.gboxAyarlarPaneli.Controls.Add(this.label1);
+            this.gboxAyarlarPaneli.Controls.Add(this.numTxtKalinlik);
+            this.gboxAyarlarPaneli.Location = new System.Drawing.Point(13, 191);
+            this.gboxAyarlarPaneli.Name = "gboxAyarlarPaneli";
+            this.gboxAyarlarPaneli.Size = new System.Drawing.Size(175, 286);
+            this.gboxAyarlarPaneli.TabIndex = 1;
+            this.gboxAyarlarPaneli.TabStop = false;
+            // 
+            // pboxKalemRengi
+            // 
+            this.pboxKalemRengi.BackColor = System.Drawing.Color.Black;
+            this.pboxKalemRengi.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pboxKalemRengi.Location = new System.Drawing.Point(104, 47);
+            this.pboxKalemRengi.Name = "pboxKalemRengi";
+            this.pboxKalemRengi.Size = new System.Drawing.Size(62, 26);
+            this.pboxKalemRengi.TabIndex = 4;
+            this.pboxKalemRengi.TabStop = false;
+            // 
+            // pboxSekilIci
+            // 
+            this.pboxSekilIci.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.pboxSekilIci.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pboxSekilIci.Location = new System.Drawing.Point(29, 164);
+            this.pboxSekilIci.Name = "pboxSekilIci";
+            this.pboxSekilIci.Size = new System.Drawing.Size(117, 98);
+            this.pboxSekilIci.TabIndex = 4;
+            this.pboxSekilIci.TabStop = false;
+            // 
+            // chbIciniDoldur
+            // 
+            this.chbIciniDoldur.AutoSize = true;
+            this.chbIciniDoldur.Location = new System.Drawing.Point(11, 88);
+            this.chbIciniDoldur.Name = "chbIciniDoldur";
+            this.chbIciniDoldur.Size = new System.Drawing.Size(93, 20);
+            this.chbIciniDoldur.TabIndex = 2;
+            this.chbIciniDoldur.Text = "İçini Doldur";
+            this.chbIciniDoldur.UseVisualStyleBackColor = true;
+            // 
+            // pboxSekilKenari
+            // 
+            this.pboxSekilKenari.BackColor = System.Drawing.Color.Black;
+            this.pboxSekilKenari.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pboxSekilKenari.Location = new System.Drawing.Point(11, 145);
+            this.pboxSekilKenari.Name = "pboxSekilKenari";
+            this.pboxSekilKenari.Size = new System.Drawing.Size(155, 135);
+            this.pboxSekilKenari.TabIndex = 3;
+            this.pboxSekilKenari.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Kalem Rengi:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Şekil Renkleri:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kalınlık:";
+            // 
+            // numTxtKalinlik
+            // 
+            this.numTxtKalinlik.Location = new System.Drawing.Point(65, 20);
+            this.numTxtKalinlik.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numTxtKalinlik.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numTxtKalinlik.Name = "numTxtKalinlik";
+            this.numTxtKalinlik.Size = new System.Drawing.Size(99, 21);
+            this.numTxtKalinlik.TabIndex = 0;
+            this.numTxtKalinlik.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // btnSerbestCizim
+            // 
+            this.btnSerbestCizim.Location = new System.Drawing.Point(13, 134);
+            this.btnSerbestCizim.Name = "btnSerbestCizim";
+            this.btnSerbestCizim.Size = new System.Drawing.Size(175, 51);
+            this.btnSerbestCizim.TabIndex = 0;
+            this.btnSerbestCizim.Text = "Serbest Çizim";
+            this.btnSerbestCizim.UseVisualStyleBackColor = true;
+            // 
+            // btnDuzCizgi
+            // 
+            this.btnDuzCizgi.Location = new System.Drawing.Point(104, 76);
+            this.btnDuzCizgi.Name = "btnDuzCizgi";
+            this.btnDuzCizgi.Size = new System.Drawing.Size(84, 51);
+            this.btnDuzCizgi.TabIndex = 0;
+            this.btnDuzCizgi.Text = "Düz Çizgi";
+            this.btnDuzCizgi.UseVisualStyleBackColor = true;
+            // 
+            // btnDaire
+            // 
+            this.btnDaire.Location = new System.Drawing.Point(13, 76);
+            this.btnDaire.Name = "btnDaire";
+            this.btnDaire.Size = new System.Drawing.Size(84, 51);
+            this.btnDaire.TabIndex = 0;
+            this.btnDaire.Text = "Daire";
+            this.btnDaire.UseVisualStyleBackColor = true;
+            // 
+            // btnUcgen
+            // 
+            this.btnUcgen.Location = new System.Drawing.Point(104, 18);
+            this.btnUcgen.Name = "btnUcgen";
+            this.btnUcgen.Size = new System.Drawing.Size(84, 51);
+            this.btnUcgen.TabIndex = 0;
+            this.btnUcgen.Text = "Üçgen";
+            this.btnUcgen.UseVisualStyleBackColor = true;
+            // 
+            // btnDortgen
+            // 
+            this.btnDortgen.Location = new System.Drawing.Point(13, 18);
+            this.btnDortgen.Name = "btnDortgen";
+            this.btnDortgen.Size = new System.Drawing.Size(84, 51);
+            this.btnDortgen.TabIndex = 0;
+            this.btnDortgen.Text = "Dötgen";
+            this.btnDortgen.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // frmPaint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.gboxAracPaneli);
+            this.Controls.Add(this.gboxCizimPaneli);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmPaint";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "v.1.0.0 - Paint Uygulaması";
+            this.gboxCizimPaneli.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pboxCizimAlani)).EndInit();
+            this.gboxAracPaneli.ResumeLayout(false);
+            this.gboxAyarlarPaneli.ResumeLayout(false);
+            this.gboxAyarlarPaneli.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxKalemRengi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxSekilIci)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxSekilKenari)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numTxtKalinlik)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gboxCizimPaneli;
+        private System.Windows.Forms.GroupBox gboxAracPaneli;
+        private System.Windows.Forms.PictureBox pboxCizimAlani;
+        private System.Windows.Forms.GroupBox gboxAyarlarPaneli;
+        private System.Windows.Forms.PictureBox pboxSekilIci;
+        private System.Windows.Forms.PictureBox pboxSekilKenari;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numTxtKalinlik;
+        private System.Windows.Forms.Button btnSerbestCizim;
+        private System.Windows.Forms.Button btnDuzCizgi;
+        private System.Windows.Forms.Button btnDaire;
+        private System.Windows.Forms.Button btnUcgen;
+        private System.Windows.Forms.Button btnDortgen;
+        private System.Windows.Forms.CheckBox chbIciniDoldur;
+        private System.Windows.Forms.PictureBox pboxKalemRengi;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
